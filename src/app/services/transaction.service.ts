fd85e03323db9268281216ab59098526919c6ef2
Tomasz Drozdowski <external.tomasz.drozdowski@kuehne-nagel.com>
2024-09-28 15:38:34 +0200

    [moneyInvested with commission calculation]

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -9,19 +9,26 @@ export class TransactionService {
   constructor(private dbService: DbService) {}
 
   addTransaction(transaction: Transaction) {
-    const currentStocks = this.dbService.getStocksValue();
+    let currentStocks = this.dbService.getStocksValue();
     let stock = currentStocks.find((s) => s.symbol === transaction.symbol);
 
     if (stock) {
-      stock.transactions.push(transaction);
+      const updatedTransactions = [...stock.transactions, transaction];
+      stock = {
+        symbol: stock.symbol,
+        transactions: updatedTransactions,
+        moneyInvested: this.calculateMoneyInvested(updatedTransactions),
+      };
+      currentStocks = currentStocks.filter(stock => stock.symbol !== stock.symbol);
+
     } else {
       stock = {
         symbol: transaction.symbol,
         transactions: [transaction],
         moneyInvested: this.calculateMoneyInvested([transaction]),
       };
-      currentStocks.push(stock);
     }
+    currentStocks.push(stock);
 
     this.dbService.updateStocks(currentStocks);
   }
@@ -29,15 +36,17 @@ export class TransactionService {
   private calculateMoneyInvested(transactions: Transaction[]): number {
     let totalBuy = 0;
     let totalSell = 0;
+    let commission = 0;
 
     transactions.forEach((transaction) => {
+      commission += transaction.commission
       if (transaction.type === 'buy') {
-        totalBuy += transaction.amount * transaction.price + transaction.commission;
+        totalBuy += transaction.amount * transaction.price;
       } else if (transaction.type === 'sell') {
-        totalSell += transaction.amount * transaction.price - transaction.commission;
+        totalSell += (transaction.amount * transaction.price);
       }
     });
 
-    return totalBuy - totalSell;
+    return totalBuy - totalSell + commission;
   }
 }
