aa2f42cdd0a45d4e59a3ba48c9fd2cc5612144c3
Tomasz Drozdowski <external.tomasz.drozdowski@kuehne-nagel.com>
2025-06-27 07:51:25 +0200

    [environment variables]

--- src/app/services/http/company-info.service.ts
+++ src/app/services/http/company-info.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
+import {environment} from "../../../environments/environment.prod";
 
 export interface CompanyInfo {
   date: string;
@@ -17,28 +18,26 @@ export interface CompanyInfo {
   providedIn: 'root',
 })
 export class CompanyInfoService {
-  private API_KEY = 'tQr6CjESc8UVhkFN4Eugr7WXpyYCu82D';
-  private BASE_URL = 'https://financialmodelingprep.com/api/v3';
 
   constructor(private http: HttpClient) {}
 
   fetchNameSuggestions(query: string): Observable<string[]> {
-    const url = `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=5&exchange=NYSE,NASDAQ&apikey=${this.API_KEY}`;
+    const url = `${environment.FINANCIAL_MODELING_API}/search?query=${query}&limit=5&exchange=NYSE,NASDAQ&apikey=${environment.STOCKS_API}`;
     return this.http.get<any[]>(url).pipe(
-      switchMap((data) => of(data.map((item) => `${item.symbol} - ${item.name}`))), // Format as "symbol - name"
+      switchMap((data) => of(data.map((item) => `${item.symbol} - ${item.name}`))),
     );
   }
 
   getCompanyInfo(symbol: string): Observable<CompanyInfo[]> {
     return this.http.get<CompanyInfo[]>(
-      `${this.BASE_URL}/key-metrics/${symbol}?limit=5&apikey=${this.API_KEY}`,
+      `${environment.FINANCIAL_MODELING_API}/key-metrics/${symbol}?limit=5&apikey=${environment.STOCKS_API}`,
     );
   }
 
   getCompanyInfoList(limit: number, page: number): Observable<any[]> {
-    const url = `${this.BASE_URL}/stock-screener?marketCapMoreThan=1000000000&limit=${limit}&offset=${
+    const url = `${environment.FINANCIAL_MODELING_API}/stock-screener?marketCapMoreThan=1000000000&limit=${limit}&offset=${
       (page - 1) * limit
-    }&apikey=${this.API_KEY}`;
+    }&apikey=${environment.STOCKS_API}`;
     return this.http.get<any[]>(url);
   }
 }
