f322db4394f211595354899fef27ad67c8970bc2
Tomasz Drozdowski <external.tomasz.drozdowski@kuehne-nagel.com>
2025-06-30 09:29:31 +0200

    auth config

--- src/app/auth/auth-config.module.ts
+++ src/app/auth/auth-config.module.ts
@@ -7,8 +7,8 @@ import { AuthModule } from 'angular-auth-oidc-client';
     AuthModule.forRoot({
       config: {
         authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_i9ivjsuMD',
-        redirectUrl: window.location.origin,
-        postLogoutRedirectUri: window.location.origin,
+        redirectUrl: 'https://main.d1kexow7pbduqr.amplifyapp.com/',
+        postLogoutRedirectUri: 'https://main.d1kexow7pbduqr.amplifyapp.com/',
         clientId: 'istc6rrsed9f2jnguse7c6pk0',
         scope: 'email openid phone',
         responseType: 'code',
